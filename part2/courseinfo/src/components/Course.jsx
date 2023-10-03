479597497a356d7d2ca6fc94253722247261249f
Dipanshu0505 <103682768+Dipanshu0505@users.noreply.github.com>
2023-10-03 08:08:29 +0530

    changed Indentations

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -20,43 +20,41 @@ const Course = (props) => {
 }
 
 const Header = (props) => {
-// console.log(props)  
-return (
-    <h3>{props.course}</h3>
-)
+    return (
+        <h3>{props.course}</h3>
+    )
 }
 
 const Part = (props) => {
-return (
-    <p>{props.name} {props.count}</p>
-)
+    return (
+        <p>{props.name} {props.count}</p>
+    )
 }
   
 const Content = (props) => {  
-return (    
-    <div>   
-    {props.parts.map(coursePart => {
-        return (
-        <Part key={coursePart.id}name={coursePart.name} count={coursePart.exercises}/>
-        )
-    })}
-    
-    
-    {/* <Part name={props.parts[0].name} count={props.parts[0].exercises}/>
-    <Part name={props.parts[1].name} count={props.parts[1].exercises}/> */}
-    {/* <Part name={props.parts[2].name} count={props.parts[2].exercises}/>
-    <Part name={props.parts[3].name} count={props.parts[3].exercises}/> */}
-    </div>
-)
+    return (    
+        <div>   
+        {props.parts.map(coursePart => {
+            return (
+            <Part key={coursePart.id}name={coursePart.name} count={coursePart.exercises}/>
+            )
+        })}
+        
+        
+        {/* <Part name={props.parts[0].name} count={props.parts[0].exercises}/>
+        <Part name={props.parts[1].name} count={props.parts[1].exercises}/> */}
+        {/* <Part name={props.parts[2].name} count={props.parts[2].exercises}/>
+        <Part name={props.parts[3].name} count={props.parts[3].exercises}/> */}
+        </div>
+    )
 }
 
 const Total = (props) => {
 
-// console.log(props.ex)
-const sumWithInitial = props.ex.reduce((accumulator, currentValue) => accumulator += currentValue.exercises, 0);
-return (
-    <p> <b>Total of {sumWithInitial} exercises</b></p>
-)
+    const sumWithInitial = props.ex.reduce((accumulator, currentValue) => accumulator += currentValue.exercises, 0);
+    return (
+        <p> <b>Total of {sumWithInitial} exercises</b></p>
+    )
 }
   
 
